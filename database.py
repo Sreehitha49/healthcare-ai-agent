07bc017d8af68e67e55c2eddf2b05cf098895d22
Sreehitha49 <152470443+Sreehitha49@users.noreply.github.com>
2026-03-06 15:34:19 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,28 @@
+%%writefile database.py
 import sqlite3
 
+# connect to database
 conn = sqlite3.connect("health.db")
+cursor = conn.cursor()
+
+# medication table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS medication(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+time TEXT
+)
+""")
+
+# fitness table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS fitness(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+steps INTEGER,
+calories INTEGER
+)
+""")
+
+conn.commit()
+
+print("Database created successfully")
